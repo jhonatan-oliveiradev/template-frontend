778abfdbbe2d9b850c695de23ed13e2954410a2a
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2023-09-18 13:07:25 -0300

    :construction: feat: initial page

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+This is a NextJS featured starter template containing the Following features and more:
+
+## Features
+
+- [Tailwind CSS](https://tailwindcss.com)
+- [TypeScript](https://www.typescriptlang.org)
+- [ESLint](https://eslint.org)
+- [Prettier](https://prettier.io/) with precommit hook
+- [Husky](https://typicode.github.io/husky/)
+- [Jest](https://jestjs.io/) and [@testing-library/react](https://testing-library.com/docs/react-testing-library)
+
 
 ## Getting Started
 
-First, run the development server:
+This is a modified version of create-next-app with multiple useful features to get started in a flash, to use this run this in terminal
+
+```bash
+git clone https://github.com/jhonatan-oliveiradev/template-frontend.git my-app
+```
+
+Then, enter the directory using:
+```bash
+cd my-app
+```
+
+Install the packages using:
+```bash
+npm i
+```
+
+Finally, run the development server:
 
 ```bash
 npm run dev
@@ -14,21 +40,4 @@ pnpm dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,126 @@
 import Image from 'next/image'
+import Link from 'next/link'
+
+const featureCard = [
+  {
+    title: 'Next.js',
+    description: 'The React Framework for Production.',
+    href: 'https://github.com/vercel/next.js/'
+  },
+  {
+    title: 'Tailwind CSS',
+    description: 'A utility-first CSS framework packed',
+    href: 'https://tailwindcss.com'
+  },
+  {
+    title: 'TypeScript',
+    description: `Type safe JavaScript at runtime.`,
+    href: 'https://www.typescriptlang.org/'
+  },
+  {
+    title: 'ESLint',
+    description: `Find and fix problems in your code`,
+    href: 'https://eslint.org/'
+  },
+  {
+    title: 'Prettier',
+    description: `An opinionated code formatter`,
+    href: 'https://prettier.io/'
+  },
+  {
+    title: 'Husky',
+    description: `Git hooks made easy`,
+    href: 'https://typicode.github.io/'
+  },
+  {
+    title: 'Jest',
+    description: `Delightful JavaScript Testing Framework with a focus on simplicity`,
+    href: 'https://jestjs.io/'
+  },
+  {
+    title: 'React Testing Library',
+    description: `Simple and complete React DOM testing utilities that encourage good testing practices.`,
+    href: 'https://testing-library.com'
+  }
+]
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="mx-auto w-full max-w-7xl px-6">
+      <div className="flex h-screen items-center justify-between flex-col">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-4xl font-bold text-violet-700 my-6">
+            Welcome to my boilerplate!
+          </h1>
+          <p>Feel free to clone it and use it in your projects.</p>
         </div>
-      </div>
+        <div className="flex flex-col mt-12">
+          <h2 className="text-2xl font-semibold">Features</h2>
+          <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-8">
+            {featureCard.map((card) => (
+              <div className="w-7xl" key={card.title}>
+                <div>
+                  <Link
+                    className="p-6 rounded-lg bg-gray-600/20 flex flex-col gap-2 hover:text-violet-700 transition-all hover:bg-gray-600/30"
+                    href={card.href}
+                    target="_blank"
+                  >
+                    <h3 className="font-bold mb-2">{card.title}</h3>
+                    <p className="text-left text- text-sm text-zinc-400 line-clamp-1">
+                      {card.description}
+                    </p>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex flex-col items-center justify-center gap-4 mt-12 pb-6">
+          <div className="flex items-center justify-center">
+            <p className="mr-2">Give a star on</p>
+            <Link
+              className="flex items-center justify-center"
+              href="https://github.com/jhonatan-oliveiradev/template-frontend"
+            >
+              <svg
+                width="25"
+                height="25"
+                viewBox="0 0 25 25"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clipPath="url(#clip0_294_22)">
+                  <path
+                    d="M9.94092 19.7646C4.94092 21.2646 4.94092 17.2646 2.94092 16.7646M16.9409 22.7646V18.8946C16.9784 18.4177 16.914 17.9384 16.7519 17.4884C16.5899 17.0384 16.3338 16.628 16.0009 16.2846C19.1409 15.9346 22.4409 14.7446 22.4409 9.28459C22.4407 7.88841 21.9036 6.54578 20.9409 5.53459C21.3968 4.31309 21.3645 2.96294 20.8509 1.76459C20.8509 1.76459 19.6709 1.41459 16.9409 3.24459C14.6489 2.62341 12.2329 2.62341 9.94092 3.24459C7.21092 1.41459 6.03092 1.76459 6.03092 1.76459C5.51729 2.96294 5.48506 4.31309 5.94092 5.53459C4.97105 6.55329 4.43344 7.90805 4.44092 9.31459C4.44092 14.7346 7.74092 15.9246 10.8809 16.3146C10.5519 16.6546 10.2982 17.06 10.1362 17.5045C9.97427 17.949 9.90773 18.4227 9.94092 18.8946V22.7646"
+                    stroke="white"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_294_22">
+                    <rect
+                      width="24"
+                      height="24"
+                      fill="white"
+                      transform="translate(0.940918 0.764648)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+            </Link>
+          </div>
+          <Link href="https://jhonatanoliveira.com" target="_blank">
+            <Image
+              src="https://www.jhonatanoliveira.com/_next/static/media/logo.d4c2b320.svg"
+              width={130}
+              height={80}
+              alt="Logotipo Jhonatan Oliveira"
+            />
+          </Link>
+        </div>
       </div>
     </main>
   )
